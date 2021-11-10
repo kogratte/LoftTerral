df974fc1b9528ecf8e59f83ad286348edafdef3c
Nicolas ISSALENE <nicolas.issalene@agapes.fr>
2021-11-10 13:09:12 +0100

    feat(init)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import { provide } from '@vue/composition-api';
+import { DefaultApolloClient } from '@vue/apollo-composable';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import apolloClient from './apollo-client';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 
@@ -12,5 +15,8 @@ new Vue({
   router,
   store,
   vuetify,
+  setup(): void {
+    provide(DefaultApolloClient, apolloClient);
+  },
   render: (h) => h(App),
 }).$mount('#app');
